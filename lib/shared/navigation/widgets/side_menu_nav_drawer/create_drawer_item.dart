a157759c89fa7799db1310860d6a97740491b448
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-08 15:15:34 +1000

    refactor(side_menu_nav_drawer): use stateless widget rather than functions for drawer item/header

--- lib/shared/navigation/widgets/side_menu_nav_drawer/create_drawer_item.dart
+++ lib/shared/navigation/widgets/side_menu_nav_drawer/create_drawer_item.dart
@@ -1,28 +1,40 @@
 part of 'side_menu_nav_drawer_widget.dart';
 
-Widget createDrawerItem({
-  required IconData icon,
-  required String name,
-  required GestureTapCallback onTap,
-  TextStyle? nameStyle,
-  Color? iconColour,
-}) {
-  return ListTile(
-    title: Row(
-      children: <Widget>[
-        Icon(
-          icon,
-          color: iconColour,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Text(
-            name,
-            style: nameStyle,
+class SideMenuNavDrawerItem extends StatelessWidget {
+  const SideMenuNavDrawerItem(
+    Key? key,
+    this.icon,
+    this.name,
+    this.onTap,
+    this.nameStyle,
+    this.iconColour,
+  ) : super(key: key);
+
+  final IconData icon;
+  final String name;
+  final GestureTapCallback onTap;
+  final TextStyle? nameStyle;
+  final Color? iconColour;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(
+            icon,
+            color: iconColour,
           ),
-        )
-      ],
-    ),
-    onTap: onTap,
-  );
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              name,
+              style: nameStyle,
+            ),
+          )
+        ],
+      ),
+      onTap: onTap,
+    );
+  }
 }
--- lib/shared/navigation/widgets/side_menu_nav_drawer/create_header.dart
+++ lib/shared/navigation/widgets/side_menu_nav_drawer/create_header.dart
@@ -1,34 +1,42 @@
 part of 'side_menu_nav_drawer_widget.dart';
 
-Widget createHeader({
-  String headerTitle = 'OncWS',
-}) {
-  return DrawerHeader(
-    margin: EdgeInsets.zero,
-    padding: EdgeInsets.zero,
-    decoration: const BoxDecoration(
-      image: DecorationImage(
-        fit: BoxFit.scaleDown,
-        image: AssetImage(
-          'assets/images/icons/logo.png',
-        ),
-      ),
-    ),
-    child: Stack(
-      children: <Widget>[
-        Positioned(
-          bottom: 16.0,
-          left: 16.0,
-          child: Text(
-            headerTitle,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 20.0,
-              fontWeight: FontWeight.w500,
-            ),
+class SideMenuNavDrawerHeader extends StatelessWidget {
+  const SideMenuNavDrawerHeader(
+    Key? key,
+    this._headerTitle,
+  ) : super(key: key);
+
+  final String _headerTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.scaleDown,
+          image: AssetImage(
+            'assets/images/icons/logo.png',
           ),
         ),
-      ],
-    ),
-  );
+      ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 16.0,
+            left: 16.0,
+            child: Text(
+              _headerTitle,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
