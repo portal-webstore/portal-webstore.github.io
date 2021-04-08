3cb93654a87ee55080f2dcf885b94eb1b598e3a7
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-08 15:46:38 +1000

    feat(side_menu): add home page to side menu list
    unfortunate that, although we know all the text and params at compile time, passing into reusable
    widget bars it from being a compile-time constant

--- lib/shared/navigation/widgets/side_menu_nav_drawer/side_menu_nav_drawer_widget.dart
+++ lib/shared/navigation/widgets/side_menu_nav_drawer/side_menu_nav_drawer_widget.dart
@@ -1,4 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:testable_web_app/shared/navigation/routes_constant.dart';
 
 part 'drawer_header.dart';
 part 'drawer_item.dart';
+
+class SideMenuNavigationDrawer extends StatelessWidget {
+  const SideMenuNavigationDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const SideMenuNavDrawerHeader(
+            title: 'Webstore',
+          ),
+          SideMenuNavDrawerItem(
+            icon: Icons.home_rounded,
+            name: 'Home',
+            onTap: () => Navigator.pushReplacementNamed(
+              context,
+              Routes.home,
+            ),
+          ),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
