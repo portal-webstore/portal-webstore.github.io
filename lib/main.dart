29b5353c6168ae85862c77a57927b8ce60be3c75
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-09 12:26:28 +1000

    feat(main,counter_observer): init counter observer to watch state changes
    - TODO: still need to nest the button under the bloc provider cubit to be able to retrieve context
    cubit from parent

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart' show runApp;
 import 'package:testable_web_app/my_app.dart' show MyApp;
+import 'package:testable_web_app/sample/counter/observers/counter_observer.dart';
 
 void main() {
+  CounterObserver();
+
   runApp(
     const MyApp(),
   );
--- lib/sample/counter/observers/counter_observer.dart
+++ lib/sample/counter/observers/counter_observer.dart
@@ -1,1 +1,15 @@
+import 'package:bloc/bloc.dart' show Bloc, BlocObserver, Transition;
+import 'package:flutter/material.dart' show debugPrint;
 
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which
+/// observes all [Bloc] state changes.
+/// {@endtemplate}
+class CounterObserver extends BlocObserver {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+
+    debugPrint('${bloc.runtimeType} $transition');
+  }
+}
