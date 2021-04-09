79c32e1a557a0824eedd0d3c5748daf56bcad840
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-09 12:35:24 +1000

    fix(counter,screens,widgets): nest button press event handler (+ whole scaffold) to access context
    Access context ReadContext for the CounterCubit in parent. Otherwise need to use different widgets
    rather than scaffold floating buttons. context.read<CounterCubit>(). TODO: Need to rename the view
    widget as it is no longer solely a text view

--- lib/sample/counter/screens/counter_screen.dart
+++ lib/sample/counter/screens/counter_screen.dart
@@ -20,62 +20,11 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter screen'),
-      ),
-      body: Center(
-        child: BlocProvider(
-          create: (_) => CounterCubit(),
-          child: CounterTextView(
-            textStyle: textTheme.headline2,
-          ),
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            key: const Key('+increment_counterScreen_floatingActionButton'),
-            heroTag: '+increment_counterScreen_floatingActionButton',
-            onPressed: () {
-              /// Provider provider.dart read(). Exposed through flutter_bloc extension
-              /// [ReadContext]
-              ///
-              /// Call read() only in event handlers like onPressed.
-              ///
-              /*
-                Widget build(BuildContext context) {
-                  counter is used only for the onPressed of RaisedButton
-
-                  final counter = context.read<Counter>();
-                  return RaisedButton(
-                    onPressed: () => counter.increment(),
-                  );
-                }
-                While this code is not bugged in itself, this is an anti-pattern. 
-                It could easily lead to bugs in the future after refactoring the widget to use 
-                counter for other things, but forget to change [read] into [watch].
-                
-                CONSIDER calling [read] inside event handlers:
-              */
-
-              context.read<CounterCubit>().increment();
-            },
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            key: const Key('-decrement_counterScreen_floatingActionButton'),
-            heroTag: '-decrement_counterScreen_floatingActionButton',
-            onPressed: () {
-              context.read<CounterCubit>().decrement();
-            },
-            child: const Icon(Icons.remove),
-          ),
-        ],
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const CounterTextView(
+        key: Key('CounterViewKey'),
+        textStyle: null,
       ),
     );
   }
--- lib/sample/counter/widgets/counter_text_view_widget.dart
+++ lib/sample/counter/widgets/counter_text_view_widget.dart
@@ -18,13 +18,63 @@ class CounterTextView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CounterCubit, int>(
-      builder: (context, state) {
-        return Text(
-          '$state',
-          style: textStyle ?? Theme.of(context).textTheme.headline2,
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter screen'),
+      ),
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text(
+              '$state',
+              style: textStyle ?? Theme.of(context).textTheme.headline2,
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            key: const Key('+increment_counterScreen_floatingActionButton'),
+            heroTag: '+increment_counterScreen_floatingActionButton',
+            onPressed: () {
+              /// Provider provider.dart read(). Exposed through flutter_bloc extension
+              /// [ReadContext]
+              ///
+              /// Call read() only in event handlers like onPressed.
+              ///
+              /*
+                Widget build(BuildContext context) {
+                  counter is used only for the onPressed of RaisedButton
+
+                  final counter = context.read<Counter>();
+                  return RaisedButton(
+                    onPressed: () => counter.increment(),
+                  );
+                }
+                While this code is not bugged in itself, this is an anti-pattern. 
+                It could easily lead to bugs in the future after refactoring the widget to use 
+                counter for other things, but forget to change [read] into [watch].
+                
+                CONSIDER calling [read] inside event handlers:
+              */
+
+              context.read<CounterCubit>().increment();
+            },
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            key: const Key('-decrement_counterScreen_floatingActionButton'),
+            heroTag: '-decrement_counterScreen_floatingActionButton',
+            onPressed: () {
+              context.read<CounterCubit>().decrement();
+            },
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
     );
   }
 }
