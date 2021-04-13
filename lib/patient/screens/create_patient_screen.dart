2d795e06e43784da5b2dc06b0fc9ac252d982be4
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-13 14:13:51 +1000

    feat(patient,screens): add base screens for patient CRUD
    Create patient. Update/delete patient on search

---
+++ lib/patient/screens/create_patient_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CreatePatientScreen extends StatelessWidget {
+  const CreatePatientScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
