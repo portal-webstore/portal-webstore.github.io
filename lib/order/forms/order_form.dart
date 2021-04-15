6cf73c0af1176eb8cd113f805591379236dabc10
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-15 11:48:18 +1000

    feat(order_form): move fields within padded scroll view. UX layout
    Text form field spacing with unused helper text

--- lib/order/forms/order_form.dart
+++ lib/order/forms/order_form.dart
@@ -4,7 +4,7 @@ import 'package:testable_web_app/order/forms/widgets/dose_field_widget.dart';
 import 'package:testable_web_app/webstore/catalogue/product/models/product_model.dart';
 import 'package:testable_web_app/webstore/catalogue/product/widgets/product_detail_widget.dart';
 
-const edgeInsetsPadding = EdgeInsets.fromLTRB(24, 16, 24, 16);
+const edgeInsetsPadding = EdgeInsets.fromLTRB(16, 16, 16, 16);
 const maxNumTextCharacters = 1000;
 
 class OrderForm extends StatefulWidget {
@@ -27,98 +27,98 @@ class _OrderFormState extends State<OrderForm> {
   Widget build(BuildContext context) {
     final ProductModel? productDetail = _productDetailsToShow;
 
-    return Column(
-      children: [
-        Container(
-          padding: edgeInsetsPadding,
-        ),
+    return SingleChildScrollView(
+      child: Container(
+        padding: edgeInsetsPadding,
+        child: Wrap(
+          spacing: 16,
+          runSpacing: 16,
+          children: <Widget>[
+            // - TODO: Replace with autocomplete
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Search patient',
+              ),
+            ),
 
-        // - TODO: Replace with autocomplete
-        TextFormField(
-          decoration: const InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'Search patient',
-            helperText: '',
-          ),
-        ),
+            ElevatedButton(
+              onPressed: () {
+                // setState isNewPatient
+              },
+              child: const Text(
+                'Create new patient',
+              ),
+            ),
 
-        ElevatedButton(
-          onPressed: () {
-            // setState isNewPatient
-          },
-          child: const Text(
-            'Create new patient',
-          ),
-        ),
+            // Could add the patient creation fields directly here for better UX
 
-        // Could add the patient creation fields directly here for better UX
+            // - TODO: Replace with product autocomplete
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Search product',
+              ),
+            ),
 
-        // - TODO: Replace with product autocomplete
-        TextFormField(
-          decoration: const InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'Search product',
-            helperText: '',
-          ),
-        ),
+            // Blank it vs possible visibility tween
+            Visibility(
+              visible: productDetail != null,
+              child: _showValidProductDetail(
+                _productDetailsToShow,
+              ),
+            ),
+            // - TODO: Replace with generated number of dose fields
+            DoseField(
+              drug: widget.products[0].drugs[0],
+            ),
 
-        // Blank it vs possible visibility tween
-        Visibility(
-          visible: productDetail != null,
-          child: _showValidProductDetail(
-            _productDetailsToShow,
-          ),
-        ),
-        // - TODO: Replace with generated number of dose fields
-        DoseField(
-          drug: widget.products[0].drugs[0],
-        ),
+            // - TODO: Replace
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Required date',
+              ),
+            ),
 
-        // - TODO: Replace
-        TextFormField(
-          decoration: const InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'Required date',
-            helperText: '',
-          ),
-        ),
+            // Free text notes
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Notes',
+                helperText: '',
+                alignLabelWithHint: true,
+              ),
+              textAlignVertical: TextAlignVertical.top,
+              // Note that shift arrow selection on Flutter web does not autoscroll
+              //
+              // Also note that multi line selection has unintuitive behaviour
+              // Selecting from a line that does not have the text caret cursor on
+              // it causes the field to scroll instead of selecting from that line
+              // * Vertical dragging gesture override seems to be the root issue
+              // You can use mouse cursor to select within one line though
+              // *and multi-lines* if you initate the cursor drag selection
+              // **horizontally**
+              //
+              // ...
+              minLines: 4,
+              maxLines: 6,
+              maxLength: maxNumTextCharacters,
+              // https://flutter.dev/docs/release/breaking-changes/use-maxLengthEnforcement-instead-of-maxLengthEnforced#default-values-of-maxlengthenforcement
+              // Composition end may not be working correctly. Appears to hard-limit
+              maxLengthEnforcement:
+                  MaxLengthEnforcement.truncateAfterCompositionEnds,
+            ),
 
-        // Free text notes
-        TextFormField(
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Notes',
-            helperText: '',
-            alignLabelWithHint: true,
-          ),
-          textAlignVertical: TextAlignVertical.top,
-          // Note that shift arrow selection on Flutter web does not autoscroll
-          //
-          // Also note that multi line selection has unintuitive behaviour
-          // Selecting from a line that does not have the text caret cursor on
-          // it causes the field to scroll instead of selecting from that line
-          // * Vertical dragging gesture override seems to be the root issue
-          // You can use mouse cursor to select within one line though
-          // *and multi-lines* if you initate the cursor drag selection
-          // **horizontally**
-          //
-          // ...
-          minLines: 4,
-          maxLines: 6,
-          maxLength: maxNumTextCharacters,
-          // https://flutter.dev/docs/release/breaking-changes/use-maxLengthEnforcement-instead-of-maxLengthEnforced#default-values-of-maxlengthenforcement
-          // Composition end may not be working correctly. Appears to hard-limit
-          maxLengthEnforcement:
-              MaxLengthEnforcement.truncateAfterCompositionEnds,
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Submit order to cart',
+              ),
+            )
+          ],
         ),
-
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text(
-            'Submit order to cart',
-          ),
-        )
-      ],
+      ),
     );
   }
 
