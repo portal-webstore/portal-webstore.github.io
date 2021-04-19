4a49191af4e9687a23060b64f7313b2313353bb4
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-19 14:11:28 +1000

    feat(cart_spreadsheet): add demo button actions for editable
    from sample

--- lib/order/forms/widgets/cart_spreadsheet_widget.dart
+++ lib/order/forms/widgets/cart_spreadsheet_widget.dart
@@ -1,3 +1,4 @@
+import 'package:editable/editable.dart';
 import 'package:flutter/material.dart';
 
 class CartSpreadsheet extends StatefulWidget {
@@ -10,8 +11,50 @@ class CartSpreadsheet extends StatefulWidget {
 }
 
 class _CartSpreadsheetState extends State<CartSpreadsheet> {
+  final _editableKey = GlobalKey<EditableState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Wrap(
+      children: [
+        TextButton.icon(
+          onPressed: () => _addNewRow(),
+          icon: const Icon(Icons.add),
+          label: const Text(
+            'Add',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        TextButton(
+          onPressed: () => _printEditedRows(),
+          child: const Text(
+            'Print edited rows',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Function to add a new row
+  /// Using the global key assigined to Editable widget
+  /// Access the current state of Editable
+  void _addNewRow() {
+    setState(() {
+      _editableKey.currentState?.createRow();
+    });
+  }
+
+  ///Print only edited rows.
+  void _printEditedRows() {
+    final List<dynamic>? editedRows = _editableKey.currentState?.editedRows;
+
+    if (editedRows == null) {
+      return;
+    }
+    print(editedRows);
   }
 }
