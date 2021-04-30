b91ab9e3855143a8f60e8b56e3516309754904fd
S web <82124333+webstoreportal@users.noreply.github.com>
2021-04-30 15:55:49 +1000

    feat(product_model): introduce new, more free-form models

--- lib/webstore/catalogue/product/models/product_model.dart
+++ lib/webstore/catalogue/product/models/product_model.dart
@@ -1,5 +1,84 @@
 import 'package:equatable/equatable.dart';
+import 'package:testable_web_app/order/forms/models/drug_dose_model.dart';
 import 'package:testable_web_app/webstore/catalogue/product/models/drug_model.dart';
+
+/// The divergence of the product models is likely going to be a source of bugs
+/// in the future
+///
+/// Add base product model reference
+/// Add ad hoc "created" semi-structured free-text product model that may be
+/// assisted through hard-coded dropdowns
+class BaseProductModel extends Equatable {
+  const BaseProductModel({
+    required this.productName,
+    required this.diluentName,
+    required this.containerName,
+    required this.drugs,
+    required this.productAdministrationRoute,
+  });
+
+  /// Simplified model has drugs and container name (diluent/type)
+  final String productName;
+
+  /// Null where not valid to have diluent name (e.g. OCS)
+  /// Empty string to indicate given as empty
+  final String? diluentName;
+
+  final String? containerName;
+
+  /// Free-text drug name and
+  /// Free-text drug units of measure
+  final List<DrugModel> drugs;
+
+  /// ITHEC IVINF IVENOS
+  final String productAdministrationRoute;
+
+  @override
+  List<Object> get props => [
+        productName,
+        diluentName ?? '',
+        containerName ?? '',
+        drugs,
+        productAdministrationRoute,
+      ];
+}
+
+/// Free-form more closely resemble the minimum subset of fields required to
+/// generate an order card with the text information displayable for
+/// reprocessing
+class TreatmentProductModel extends Equatable {
+  const TreatmentProductModel({
+    required this.productName,
+    required this.diluentName,
+    required this.containerName,
+    required this.drugDoses,
+    required this.productAdministrationRoute,
+  });
+
+  /// Product name concatenates up the diluent container parts.
+  final String productName;
+
+  /// Null where not valid to have diluent name (e.g. OCS)
+  /// Empty string to indicate given as empty
+  final String? diluentName;
+
+  /// All current cases should have a containerName
+  final String? containerName;
+
+  final List<DrugDose> drugDoses;
+
+  /// ITHEC IVINF IVENOS
+  final String productAdministrationRoute;
+
+  @override
+  List<Object> get props => [
+        productName,
+        diluentName ?? '',
+        containerName ?? '',
+        drugDoses,
+        productAdministrationRoute,
+      ];
+}
 
 /// Simplified view model of the product data required to display and
 /// submit an order for this specific product
